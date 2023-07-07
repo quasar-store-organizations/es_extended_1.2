1f4a22bcb3cd4724c87c91451ee847d3278fced6
Clefas <61330202+Clefas@users.noreply.github.com>
2023-07-07 15:38:09 -0300

    MODIFIED FILES

--- server/common.lua
+++ server/common.lua
@@ -19,33 +19,25 @@ function getSharedObject()
 end
 
 MySQL.ready(function()
-	MySQL.Async.fetchAll('SELECT * FROM items', {}, function(result)
-		for k,v in ipairs(result) do
-			ESX.Items[v.name] = {
-				label = v.label,
-				weight = v.weight,
-				rare = v.rare,
-				canRemove = v.can_remove
-			}
-		end
-	end)
+	ESX.Items = exports['qs-inventory']:GetItemList()
 
 	MySQL.Async.fetchAll('SELECT * FROM jobs', {}, function(jobs)
-		for k,v in ipairs(jobs) do
+		for k, v in ipairs(jobs) do
 			ESX.Jobs[v.name] = v
 			ESX.Jobs[v.name].grades = {}
 		end
 
 		MySQL.Async.fetchAll('SELECT * FROM job_grades', {}, function(jobGrades)
-			for k,v in ipairs(jobGrades) do
+			for k, v in ipairs(jobGrades) do
 				if ESX.Jobs[v.job_name] then
 					ESX.Jobs[v.job_name].grades[tostring(v.grade)] = v
 				else
-					print(('[es_extended] [^3WARNING^7] Ignoring job grades for "%s" due to missing job'):format(v.job_name))
+					print(('[es_extended] [^3WARNING^7] Ignoring job grades for "%s" due to missing job'):format(v
+					.job_name))
 				end
 			end
 
-			for k2,v2 in pairs(ESX.Jobs) do
+			for k2, v2 in pairs(ESX.Jobs) do
 				if ESX.Table.SizeOf(v2.grades) == 0 then
 					ESX.Jobs[v2.name] = nil
 					print(('[es_extended] [^3WARNING^7] Ignoring job "%s" due to no job grades found'):format(v2.name))
@@ -53,6 +45,11 @@ MySQL.ready(function()
 			end
 		end)
 	end)
+
+		
+	ESX.StartDBSync()
+	ESX.StartPayCheck()
+
 
 	print('[es_extended] [^2INFO^7] ESX developed by ESX-Org has been initialized')
 end)
